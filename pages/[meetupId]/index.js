3643008ce1c3c728c5e33efc6dd93d95e0ddba54
Leticia Somera <leticia.somera@gmail.com>
2022-05-26 11:24:27 -0500

    fixed fallback blocking

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -38,7 +38,7 @@ export async function getStaticPaths() {
       client.close();
 
     return {
-        fallback: 'true',
+        fallback: 'blocking',
         paths: meetups.map((meetups) => ({
             params: {
                 meetupId: meetups._id.toString()
